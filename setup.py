7321af01321f0c8ad641ad507037101fc9db4698
sensei100 <tuh42082@temple.edu>
2021-12-15 15:40:18 -0500

    Update version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 import setuptools
 from setuptools.command.install import install
 
-VERSION = "v0.8.4"
+VERSION = "v0.8.5"
 
 with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()

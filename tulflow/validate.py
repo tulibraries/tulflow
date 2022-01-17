6842e96b1094b0b0a413e2c6266b249094215805
Jen <tuh42082@temple.edu>
2022-01-17 10:26:27 -0500

    DEVO-360 Airflow 2.0 work (#168)
    * DEVO-360 Airflow 2.0 work
    
    - Adds backport packages needed for the upgrade to Airflow 2.0
    - Pins some of the dependencies that currently have breaking changes in newer versions
    
    * Add mock
    
    * Update python orb
    
    * try pytest
    
    * Update config
    
    * Use --dev
    
    * import mock
    
    * import mock in tests
    
    * use unittest.mock

--- tulflow/validate.py
+++ tulflow/validate.py
@@ -1,10 +1,9 @@
 """Generic Data (primarily XML & JSON) Validation Methods."""
-from airflow import AirflowException
+import logging
 import csv
 import io
+from airflow import AirflowException
 from lxml import etree, isoschematron
-import logging
-import re
 from tulflow import process
 
 

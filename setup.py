2e718ca722716a8f26de4e351631a9ef35239cae
Jen <tuh42082@temple.edu>
2024-02-20 14:14:19 -0500

    Update airflow to 2.8.1 (#620)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 import setuptools
 from setuptools.command.install import install
 
-VERSION = "v0.9.7"
+VERSION = "v0.9.8"
 
 with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
